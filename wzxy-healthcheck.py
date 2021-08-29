ded3f86f01dfaaeff029e68fea78c75fe0ea60a6
wgscjnb <75006420+wgscjnb@users.noreply.github.com>
2021-08-29 14:12:13 +0800

    Update wzxy-healthcheck.py

--- wzxy-healthcheck.py
+++ wzxy-healthcheck.py
@@ -81,7 +81,7 @@ class WoZaiXiaoYuanPuncher:
         self.header['Content-Type'] = "application/x-www-form-urlencoded"
         self.header['JWSESSION'] = self.getJwsession()
         sign_data = {
-            "answers": '["0","1","1"]',
+            "answers": '["0"]',
             "latitude": os.environ['WZXY_LATITUDE'],
             "longitude": os.environ['WZXY_LONGITUDE'],
             "country": os.environ['WZXY_COUNTRY'],
