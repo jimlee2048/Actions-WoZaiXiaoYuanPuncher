b48f79954933d0be79d815f4a6c4d0e07531ed91
Jim Lee <jimlee2002@outlook.com>
2022-01-20 18:05:53 +0800

    style: optimize notify result print

--- wzxy-dailyreport.py
+++ wzxy-dailyreport.py
@@ -201,7 +201,7 @@ class WoZaiXiaoYuanPuncher:
                 "desp": "打卡项目：日检日报\n\n打卡情况：{}\n\n打卡时段：{}\n\n打卡时间：{}".format(notifyResult, notifySeq, notifyTime)
             }
             requests.post(url.format(notifyToken), data=body)
-            print("消息经Serverchan-Turbo推送成功")
+            print("消息已通过 Serverchan-Turbo 推送，请检查推送结果")
         if os.environ.get('PUSHPLUS_TOKEN'):
             # pushplus 推送
             url = 'http://www.pushplus.plus/send'
@@ -222,9 +222,22 @@ class WoZaiXiaoYuanPuncher:
             headers = {'Content-Type':'application/json'}
             r = requests.post(url, data=body, headers=headers).json()
             if r["code"] == 200:
-                print("消息经pushplus推送成功")
+                print("消息经 pushplus 推送成功")
             else:
-                print("消息经pushplus推送失败，请检查token是否配置正确")
+                print("pushplus: " + r)
+                print("消息经 pushplus 推送失败，请检查错误信息")
+        if os.environ.get('GOBOT_URL'):
+            # go_cqhttp 推送
+            GOBOT_URL = os.environ["GOBOT_URL"]
+            GOBOT_TOKEN = os.environ["GOBOT_TOKEN"]
+            GOBOT_QQ = os.environ["GOBOT_QQ"]
+            url = f'{GOBOT_URL}?access_token={GOBOT_TOKEN}&{GOBOT_QQ}&message=⏰ 我在校园打卡结果通知\n---------\n\n打卡项目：健康打卡\n\n打卡情况：{notifyResult}\n\n打卡时间: {notifyTime}'
+            r = requests.get(url).json()
+            if r["status"] == "ok":
+                print("消息经 go-cqhttp 推送成功！")
+            else:
+                print("go-cqhttp:" + r)
+                print("消息经 go-cqhttp 推送失败，请检查错误信息")
         if os.environ.get('DD_BOT_ACCESS_TOKEN'):
             # 钉钉推送
             DD_BOT_ACCESS_TOKEN = os.environ["DD_BOT_ACCESS_TOKEN"]
@@ -242,11 +255,12 @@ class WoZaiXiaoYuanPuncher:
                 'msgtype': 'text',
                 'text': {'content': f'⏰ 我在校园打卡结果通知\n---------\n打卡项目：日检日报\n\n打卡情况：{notifyResult}\n\n打卡时间: {notifyTime}'}
             }
-            response = requests.post(url=url, data=json.dumps(data), headers=headers, timeout=15).json()
-            if not response['errcode']:
-                print('消息经钉钉机器人推送成功！')
+            r = requests.post(url=url, data=json.dumps(data), headers=headers, timeout=15).json()
+            if not r['errcode']:
+                print('消息经 钉钉机器人 推送成功！')
             else:
-                print('消息经钉钉机器人推送失败！')
+                print("dingding:" + r)
+                print('消息经 钉钉机器人 推送失败，请检查错误信息')
         if os.environ.get('BARK_TOKEN'):
             # bark 推送
             notifyToken = os.environ['BARK_TOKEN']
@@ -260,7 +274,7 @@ class WoZaiXiaoYuanPuncher:
                 "text": "打卡项目：日检日报\n\n打卡情况：{}\n\n打卡时段：{}\n\n打卡时间：{}".format(notifyResult, notifySeq, notifyTime)
             }
             requests.post(baseurl, data=body)
-            print("消息经喵推送推送成功")
+            print("消息已通过 喵推送 进行通知，请检查推送结果")
 
 
 if __name__ == '__main__':
--- wzxy-healthcheck.py
+++ wzxy-healthcheck.py
@@ -154,7 +154,7 @@ class WoZaiXiaoYuanPuncher:
                 "desp": "打卡项目：健康打卡\n\n打卡情况：{}\n\n打卡时间：{}".format(notifyResult, notifyTime)
             }
             requests.post(url.format(notifyToken), data=body)
-            print("消息已使用 Serverchan-Turbo 推送，请检查推送结果")
+            print("消息已通过 Serverchan-Turbo 推送，请检查推送结果")
         if os.environ.get('PUSHPLUS_TOKEN'):
             # pushplus 推送
             url = 'http://www.pushplus.plus/send'
@@ -176,7 +176,7 @@ class WoZaiXiaoYuanPuncher:
             if r["code"] == 200:
                 print("消息经 pushplus 推送成功")
             else:
-                print(r)
+                print("pushplus: " + r)
                 print("消息经 pushplus 推送失败，请检查错误信息")
         if os.environ.get('GOBOT_URL'):
             # go_cqhttp 推送
@@ -188,7 +188,7 @@ class WoZaiXiaoYuanPuncher:
             if r["status"] == "ok":
                 print("消息经 go-cqhttp 推送成功！")
             else:
-                print(r)
+                print("go-cqhttp:" + r)
                 print("消息经 go-cqhttp 推送失败，请检查错误信息")
         if os.environ.get('DD_BOT_ACCESS_TOKEN'):
             # 钉钉推送
@@ -211,7 +211,7 @@ class WoZaiXiaoYuanPuncher:
             if not r['errcode']:
                 print('消息经 钉钉机器人 推送成功！')
             else:
-                print(r)
+                print("dingding:" + r)
                 print('消息经 钉钉机器人 推送失败，请检查错误信息')
         if os.environ.get('BARK_TOKEN'):
             # bark 推送
